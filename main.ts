abcf5b4e97e89c81b93007a416d23fc176d4749a
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-19 13:28:03 -0400

    edit-not finished

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Julianne Leblanc-Peltier
+ * Created on: Sep 2023
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('the area of a rectangle with 5cm x 3cm measurements is:' + (5 * 3).toString())
+/* expected to print '15' as the area*/
+basic.showString('cm.')
+basic.showString('The perimeter is:' + (2 * 5 + 2 * 3).toString())
+/* expected to print '16' as the perimeter */
+basic.showString('cm')
