3e45b107a62f523669b599a35fc15604ca636509
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-20 00:24:04 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,22 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Julianne Leblanc-Peltier
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import sleep
 
 
-display.scroll("Hello, World!")
+display.clear()
+sleep(1)
+
+display.scroll("the area of a rectangle with 5cm x 3cm measurements is: " + str(5 * 3))
+display.scroll("cm.")
+sleep(0.5)
+display.clear()
+
+display.scroll("The perimeter is: " + str(2 * 5 + 2 * 3))
+display.scroll("cm.")
+sleep(0.5)
+display.clear()
