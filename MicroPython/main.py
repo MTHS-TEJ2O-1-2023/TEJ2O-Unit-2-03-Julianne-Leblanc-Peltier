d5fc7e3466f77ea95b0df008a50d028307cbff60
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-21 01:54:51 +0000

    YAYY!! IT WORKED!!! (Right..?)

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -11,12 +11,13 @@ from time import sleep
 display.clear()
 sleep(1)
 
-display.scroll("the area of a rectangle with 5cm x 3cm measurements is: " + str(5 * 3))
-display.scroll("cm.")
+display.scroll("the area of a rectangle with 5cm x 3cm measurements is:" + str(5 * 3))
+display.scroll("cm^2.")
 sleep(0.5)
 display.clear()
 
-display.scroll("The perimeter is: " + str(2 * 5 + 2 * 3))
+display.scroll("The perimeter is:" + str(2 * (5 + 3)))
 display.scroll("cm.")
+display.show(Image.HAPPY)
 sleep(0.5)
 display.clear()
